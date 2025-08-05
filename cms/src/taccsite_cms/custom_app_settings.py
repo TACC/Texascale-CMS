5f745b5cc034c75d7fe8fef0fda0e4af33a24a77
Wesley B <62723358+wesleyboar@users.noreply.github.com>
2025-08-05 16:23:44 -0500

    refactor: commit INSTALLED_APPS for blog (#23)

---
+++ cms/src/taccsite_cms/custom_app_settings.py
@@ -0,0 +1,13 @@
+CUSTOM_APPS = [
+
+    # DJANGOCMS_BLOG
+    'parler',
+    'taggit',
+    'taggit_autosuggest',
+    'sortedm2m',
+    'djangocms_blog',
+
+]
+
+CUSTOM_MIDDLEWARE = []
+STATICFILES_DIRS = ()
