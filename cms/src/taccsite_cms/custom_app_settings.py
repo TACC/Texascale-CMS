03df65ba0ba7b80d0e93b0c2f948aa41a1676822
Wesley B <62723358+wesleyboar@users.noreply.github.com>
2025-08-05 15:41:22 -0500

    fix: remove code forma nother branch

--- cms/src/taccsite_cms/custom_app_settings.py
+++ cms/src/taccsite_cms/custom_app_settings.py
@@ -1,12 +1,4 @@
-# import os
-
-# BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
 CUSTOM_APPS = [
-
-    # TEXASCALE_CUSTOM
-    # !!!: Independent app to customize site cuz texascale_cms did not let me
-    'texascale_custom.apps.TexascaleCustomConfig',
 
     # DJANGOCMS_BLOG
     'parler',
@@ -18,7 +10,4 @@ CUSTOM_APPS = [
 ]
 
 CUSTOM_MIDDLEWARE = []
-
-# STATICFILES_DIRS = (
-#     os.path.join(BASE_DIR, 'texascale_custom', 'static'),
-# )
+STATICFILES_DIRS = ()
