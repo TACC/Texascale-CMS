0a87ca9dc6877d59c7bf8d177f9d0b476b863f9e
Wesley B <62723358+wesleyboar@users.noreply.github.com>
2025-08-13 16:55:06 -0500

    REFACTOR: IT ALL WORKS, with unexpected UI

--- cms/src/taccsite_custom/taccsite_bootstrap4_tabs/extend.py
+++ cms/src/taccsite_custom/taccsite_bootstrap4_tabs/extend.py
@@ -5,10 +5,9 @@ logger = logging.getLogger(f"portal.{__name__}")
 def extendBootstrap4TabsPlugin():
     from django import forms
     from django.utils.translation import gettext_lazy as _
+    from django.contrib import admin
 
     from cms.plugin_pool import plugin_pool
-
-    from filer.models import Image
 
     # djangocms_bootstrap4
     from djangocms_bootstrap4.contrib.bootstrap4_tabs.cms_plugins import (
@@ -17,53 +16,31 @@ def extendBootstrap4TabsPlugin():
 
     from .models import Bootstrap4TabItem, TabImageExtension
 
-    class Bootstrap4TabItemForm(forms.ModelForm):
-        tab_image = forms.ImageField(
-            label=_('Tab Image/Thumbnail'),
-            required=False,
-        )
-
-        class Meta:
-            model = Bootstrap4TabItem
-            fields = ('tab_title',)
-
-        def __init__(self, *args, **kwargs):
-            super().__init__(*args, **kwargs)
-            if self.instance and self.instance.pk:
-                try:
-                    extension = getattr(self.instance, 'image_extension', None)
-                    if extension:
-                        self.fields['tab_image'].initial = extension.tab_image
-                except TabImageExtension.DoesNotExist:
-                    # This is a legacy tab item created before the extension
-                    pass
-
-        def save(self, commit=True):
-            instance = super().save(commit=False)
-            if commit:
-                instance.save()
-                if 'tab_image' in self.cleaned_data and self.cleaned_data['tab_image']:
-                    extension, _ = TabImageExtension.objects.get_or_create(
-                        bootstrap4_tab_item=instance
-                    )
-                    extension.tab_image = self.cleaned_data['tab_image']
-                    extension.save()
-            return instance
+    class TabImageExtensionInline(admin.StackedInline):
+        """
+        Inline admin for TabImageExtension that appears within the main plugin form.
+        This gives us the proper Filer image picker without fieldset validation issues.
+        """
+        model = TabImageExtension
+        extra = 0  # Don't show extra empty forms
+        max_num = 1  # Only allow one image extension per tab
+        can_delete = False  # Don't allow deletion of the extension
+        
+        # Use all fields from the extension model
+        fields = ('tab_image',)
 
     class Bootstrap4TabItemPlugin(OriginalBootstrap4TabItemPlugin):
         model = Bootstrap4TabItem
-        form = Bootstrap4TabItemForm
         name = "Tab Item (supports Image)"
-
+        
+        # Add the inline for the extension model
+        inlines = [TabImageExtensionInline]
+        
+        # Only show the main model fields in fieldsets
         fieldsets = [
             (None, {
                 'fields': (
                     'tab_title',
-                )
-            }),
-            (_('Image'), {
-                'fields': (
-                    'tab_image',
                 )
             }),
             (_('Advanced settings'), {
