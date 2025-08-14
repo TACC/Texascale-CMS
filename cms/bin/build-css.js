110fe1e3270ad401494910ca7e431ce3a78610c9
Wesley B <62723358+wesleyboar@users.noreply.github.com>
2025-08-14 18:28:22 -0500

    feat: quiet css build

--- cms/bin/build-css.js
+++ cms/bin/build-css.js
@@ -6,8 +6,16 @@ const ROOT = __dirname + '/../';
 const BUILD_ID = process.env.BUILD_ID || process.env.npm_package_version;
 const ASSET_PATH = ROOT + 'src/taccsite_custom/texascale_cms/static/texascale_cms/';
 
+const argv = process.argv.slice(2);
+
+const supportedVerboseFlags = [
+  '--quiet', '--silent', '--no-verbose'
+];
+const hasVerboseFlag = argv.some(flag => supportedVerboseFlags.includes(flag));
+const verbose = (!hasVerboseFlag);
+
 const options = {
-  verbose: true,
+  verbose,
   fileExt: '.css',
   buildId: BUILD_ID,
   // If custom configuration is desired, then create and pass this file
